a504ab13a04a06d8da1c409af89253e35c034c53
Ed Casillas <eduardo_casillas@hotmail.com>
2025-05-01 15:04:40 -0700

    Created Android project for STB2CSharp

---
+++ STB2CSharpAndroid/app/build.gradle.kts
@@ -0,0 +1,60 @@
+﻿plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.ecasillas.stb2csharpandroid"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ecasillas.stb2csharpandroid"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        externalNativeBuild {
+            cmake {
+                cppFlags ""
+            }
+        }
+        ndk {
+            abiFilters "armeabi-v7a", "arm64-v8a"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
